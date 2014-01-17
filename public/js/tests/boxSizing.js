0e01975bc427dd73f457261ac294179094ef28d7
pothead7 <pothead7@users.noreply.github.com>
2014-01-17 10:20:26 +0100

    Create boxSizing.js

---
+++ public/js/tests/boxSizing.js
@@ -0,0 +1,14 @@
+define('tests/boxSizing', function () {
+    function boxSizingTest () {
+        this.retVal = {
+            'name': 'CSS Basic User Interface: box-sizing',
+            'url': 'http://dev.w3.org/csswg/css-ui/#box-sizing'
+
+        };
+        this.performTest = function () {
+            this.retVal.result = ('box-sizing' in document.body.style) ? true : false;
+            return this.retVal;
+        }
+    }
+    return boxSizingTest;
+});
