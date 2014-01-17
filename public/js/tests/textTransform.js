c927e2df835fa23e58108be2d58f8d8a24cc537c
pothead7 <pothead7@users.noreply.github.com>
2014-01-17 10:21:21 +0100

    Create textTransform.js

---
+++ public/js/tests/textTransform.js
@@ -0,0 +1,14 @@
+define('tests/textTransform', function () {
+    function textTransformTest () {
+        this.retVal = {
+            'name': 'CSS Text: text-transform',
+            'url': 'http://dev.w3.org/csswg/css-text/#text-transform-property'
+
+        };
+        this.performTest = function () {
+            this.retVal.result = ('text-transform' in document.body.style) ? true : false;
+            return this.retVal;
+        }
+    }
+    return textTransformTest;
+});
