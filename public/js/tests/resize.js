13a8056e28f0d1ad99ce850b97886c4078a9429e
pothead7 <pothead7@users.noreply.github.com>
2014-01-17 10:20:57 +0100

    Create resize.js

---
+++ public/js/tests/resize.js
@@ -0,0 +1,14 @@
+define('tests/resize', function () {
+    function resizeTest () {
+        this.retVal = {
+            'name': 'CSS Basic User Interface: resize',
+            'url': 'http://dev.w3.org/csswg/css-ui/#resize'
+
+        };
+        this.performTest = function () {
+            this.retVal.result = ('resize' in document.body.style) ? true : false;
+            return this.retVal;
+        }
+    }
+    return resizeTest;
+});
