4af37d828b984632e68c7a72db8290ee4e3767b2
pothead7 <pothead7@users.noreply.github.com>
2014-01-17 10:19:40 +0100

    Create borderColor.js

---
+++ public/js/tests/borderColor.js
@@ -0,0 +1,14 @@
+define('tests/borderColor', function () {
+    function borderColorTest () {
+        this.retVal = {
+            'name': 'CSS Backgrounds: border-color',
+            'url': 'http://dev.w3.org/csswg/css-backgrounds/#the-border-color'
+
+        };
+        this.performTest = function () {
+            this.retVal.result = ('border-color' in document.body.style) ? true : false;
+            return this.retVal;
+        }
+    }
+    return borderColorTest;
+});
